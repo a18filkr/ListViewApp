86dfa4e3d5e1b6f984adcbe642ad94528481869e
Filip <a18filkr@student.his.se>
2019-04-09 14:58:54 +0200

    Listview med text_items skapad Bytt namn på appen

--- app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
+++ app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
@@ -2,8 +2,11 @@ package org.brohede.marcus.listviewapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -40,5 +43,18 @@ public class MainActivity extends AppCompatActivity {
         //    See: https://material.io/guidelines/components/lists.html#lists-specs
         //    Look for "singel line specs" for "text only" lists and modify the list_item_textview
         //    accordingly
+
+        String[] rawData = {"Leif", "Berit", "Börje"};
+
+        List<String> listData = new ArrayList<String>(Arrays.asList(rawData));
+
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),R.layout.list_item_textview,
+                R.id.my_item_textview, listData);
+
+        ListView myListView = (ListView)findViewById(R.id.my_listview);
+        myListView.setAdapter(adapter);
+
+        adapter.add("Hilding");
+        adapter.add("Krusell");
     }
 }
